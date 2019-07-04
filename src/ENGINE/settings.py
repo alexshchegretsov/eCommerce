927f85a103e8b15aba5a0f3dbdde403cee11f26c
alex_shchegretsov <nydollz77@gmail.com>
2019-07-04 17:36:57 +0300

    Add login - it works

--- src/ENGINE/settings.py
+++ src/ENGINE/settings.py
@@ -104,4 +104,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 AUTH_USER_MODEL = 'accounts.User'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'to_home_url'
